805cf9ddfc975e5e1d33ece0d21bb02d5b5a2198
Philip Butland <Philip_Butland@yahoo.com>
2023-01-07 17:42:05 +0100

    up to iteration 3

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,27 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+
+router.get('/celebrities/create',(req,res)=>{
+    res.render('celebrities/new-celebrity')
+})
+
+router.post('/celebrities/create',(req,res)=>{
+    console.log(req.body)
+    const {name,occupation,catchPhrase} = req.body
+    Celebrities.create({name:name,occupation:occupation,catchPhrase:catchPhrase})
+    res.redirect('/celebrities')
+})
+
+
+// router.get('/celebrities',(req,res)=>{
+//     Celebrities.find()
+//     .then((result)=>{
+//         console.log(result)
+//         res.render('celebrities/celebrities',{result})
+//     })
+// })
+
+
+module.exports = router;
