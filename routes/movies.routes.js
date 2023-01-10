af59e3fca94b345b0914621bdc23049fd917ee4e
artweise <art2wsxweise7ujm@gmail.com>
2023-01-10 22:34:27 +0200

    started iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,4 +3,21 @@ const router = require("express").Router();
 
 // all your routes here
 
+router.get("/movies/create", (req, res) => {
+  res.render("movies/new-movie");
+});
+
+router.post("/movies/create", (req, res) => {
+  console.log(req.body);
+  const { title, genre, plot, cast } = req.body;
+  Movies.create({ title: title, genre: genre, plot: plot, cast: cast })
+    .then((result) => {
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.redirect("/new-movie");
+    });
+});
+
 module.exports = router;
